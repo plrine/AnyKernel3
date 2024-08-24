40802909367ed522756d0f268139e9d74174f675
Raya Mahardika <rayapkun@gmail.com>
2024-08-24 20:43:37 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -28,5 +28,8 @@ RAMDISK_COMPRESSION=auto;
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
